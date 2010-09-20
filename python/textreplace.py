6693f6382df04a82766b6e5bdf18ea5c315aaebd
Kyle Cheung <hello@kylezoa.com>
2010-09-19 19:58:46 -0700

    add textreplace.py -- directory functions

---
+++ python/textreplace.py
@@ -0,0 +1,23 @@
+#for only kylezoa.com html files
+#because hosts change and things get annoying with static files
+
+import os, sys, re
+from time import sleep
+
+#strings
+strFind = '''<a href="http://www.wilsonadams.net">Host by Wilson Adams Design</a><br/>'''
+strToRe = '''<a href="https://clients.thrustvps.com/aff.php?aff=128">Host by ThrustVPS</a><br/>'''
+    
+#input wanted directory
+path = input ("directory: ")
+
+#change to <path>
+os.chdir(path)
+#confirm if directory is correct
+print ("Making changes in", os.getcwd(), "you have three seconds to kill")
+sleep (3)
+
+#list then find .html files
+
+os.listdir(path)
+
