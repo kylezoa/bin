1c4193703bd42703a80a33b7e34bdb6e0ec38e8b
Kyle Cheung <hello@kylezoa.com>
2010-09-21 21:30:12 -0700

    add textreplace.py base and tested concession

--- python/textreplace.py
+++ python/textreplace.py
@@ -5,8 +5,8 @@ import os, sys, re
 from time import sleep
 
 #strings
-strFind = '''<a href="http://www.wilsonadams.net">Host by Wilson Adams Design</a><br/>'''
-strToRe = '''<a href="https://clients.thrustvps.com/aff.php?aff=128">Host by ThrustVPS</a><br/>'''
+strFind = "<a href="http://www.wilsonadams.net">Host by Wilson Adams Design</a><br/>"
+strToRe = "<a href="https://clients.thrustvps.com/aff.php?aff=128">Host by ThrustVPS</a><br/>"
     
 #input wanted directory
 path = input ("directory: ")
@@ -17,7 +17,7 @@ os.chdir(path)
 print ("Making changes in", os.getcwd(), "you have three seconds to kill")
 sleep (3)
 
-#list then find .html files
+#list then find .html files and assemble it into a list to edit
 
 os.listdir(path)
 
